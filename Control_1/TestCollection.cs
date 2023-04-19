dc8df4a24b6a1867a8164be9b00ba4df5a7ae279
Roman <roman-maslov423@yandex.ru>
2023-04-20 02:29:52 +0300

    za chto?

--- Control_1/TestCollection.cs
+++ Control_1/TestCollection.cs
@@ -6,15 +6,39 @@ using System.Threading.Tasks;
 
 namespace Control_1
 {
-    public delegate KeyValuePair<@int, TValue> GenerateElement<@int, TValue>(int j);
+    public delegate KeyValuePair<TKey,TValue> GenerateElement<TKey, TValue>(int j);
 
-    class TestCollection<int, Student>
+    class TestCollection<TKey, TValue>
     {
-        private List<int> keyList;
-        private List<Student> valueList;
-        private Dictionary<int, Student> dictionary;
-        private Dictionary<string, Student> dictionary2;
-        private GenerateElement<int,Student> generateElement;
+        private List<TKey> keyList;
+        private List<TValue> valueList;
+        private Dictionary<TKey, TValue> dictionary;
+        private Dictionary<string, TValue> dictionary2;
+        private GenerateElement<TKey,TValue> generateElement;
+
+        public TestCollection(int j) 
+        {
+            keyList = new List<TKey>(j);
+            valueList = new List<TValue>(j);
+            dictionary = new Dictionary<TKey, TValue>(j);
+            dictionary2 = new Dictionary<string, TValue>(j);
+        }
+
+        static public KeyValuePair<int, Student> whatsIt(int a)
+        {
+            return new KeyValuePair<int, Student>(a, new Student());
+        }
+
+        public TestCollection(int j, GenerateElement<TKey,TValue> generateElement) 
+        {
+            keyList = new List<TKey>(j);
+            valueList = new List<TValue>(j);
+            dictionary = new Dictionary<TKey, TValue>(j);
+            dictionary2 = new Dictionary<string, TValue>(j);
+            generateElement(j);
+        }
+
+
 
 
     }
